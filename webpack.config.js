92407cfd7189d0521c439df17bfa775f946ab6b9
Elias <yambee-star@mail.ru>
2021-06-30 06:38:36 +0300

    production

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
-  mode: 'development',
+  mode: NODE_ENV,
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  watch: true,
+  watch: NODE_ENV === 'development',
   watchOptions: {
     ignored: /node_modules/,
     poll: 1000,
@@ -54,4 +56,5 @@ module.exports = {
     hot: true,
     historyApiFallback: true,
   },
+  devtool: NODE_ENV === 'development' ? 'source-map' : false,
 };
