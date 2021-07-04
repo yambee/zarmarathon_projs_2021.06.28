3e70ce4a34feb95631fa9a99512f9ca80b3b8a2d
Elias <yambee-star@mail.ru>
2021-07-04 22:55:18 +0300

    Homework-1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './index.scss';
+import Female1Walk from './assets/Female-1-Walk.png';
+// import Male5Walk from './assets/Male-5-Walk.png';
 
-console.log('#### HI ');
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+const spriteW = 48;
+const spriteH = 48;
+const shots = 3;
+let cycle = 0;
+
+const img = document.createElement('img');
+img.src = Female1Walk;
+
+img.addEventListener('load', () => {
+  setInterval(() => {
+    cycle = (cycle + 1) % shots;
+    ctx.clearRect(0, 0, 600, 600);
+    ctx.drawImage(img, cycle * spriteW, 0, spriteW, spriteH, 0, 0, 100, 100);
+  }, 120);
+});
