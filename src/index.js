343de3b078114915aebcf4da4731c88e33201745
Elias <yambee-star@mail.ru>
2021-07-04 23:43:21 +0300

    Homework-1 hotfix-1

--- src/index.js
+++ src/index.js
@@ -9,14 +9,70 @@ const spriteW = 48;
 const spriteH = 48;
 const shots = 3;
 let cycle = 0;
+let bottomPressed = false;
+let topPressed = false;
+let rightPressed = false;
+let leftPressed = false;
+let pY = 0;
+let pX = 0;
+let rowY = 0;
+const atlasRows = {
+  down: 0,
+  left: 1,
+  right: 2,
+  up: 3,
+};
+
+// центрируем персонажа по канвасу
+pX = 300;
+pY = 300;
+
+const keyDownHandler = (e) => {
+  if (e.key === 'Down' || e.key === 'ArrowDown') bottomPressed = true;
+  if (e.key === 'Up' || e.key === 'ArrowUp') topPressed = true;
+  if (e.key === 'Right' || e.key === 'ArrowRight') rightPressed = true;
+  if (e.key === 'Left' || e.key === 'ArrowLeft') leftPressed = true;
+};
+
+const keyUpHandler = (e) => {
+  if (e.key === 'Down' || e.key === 'ArrowDown') bottomPressed = false;
+  if (e.key === 'Up' || e.key === 'ArrowUp') topPressed = false;
+  if (e.key === 'Right' || e.key === 'ArrowRight') rightPressed = false;
+  if (e.key === 'Left' || e.key === 'ArrowLeft') leftPressed = false;
+};
+
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
 
 const img = document.createElement('img');
 img.src = Female1Walk;
 
 img.addEventListener('load', () => {
   setInterval(() => {
-    cycle = (cycle + 1) % shots;
+    if (bottomPressed) {
+      if (pY <= 600 - 48 - 10) pY += 10;
+      rowY = spriteH * atlasRows.down;
+      cycle = (cycle + 1) % shots;
+    }
+    if (topPressed) {
+      if (pY >= 10) pY -= 10;
+      rowY = spriteH * atlasRows.up;
+      cycle = (cycle + 1) % shots;
+    }
+    if (rightPressed) {
+      if (pX < 600 - 48) pX += 10;
+      rowY = spriteH * atlasRows.right;
+      cycle = (cycle + 1) % shots;
+    }
+    if (leftPressed) {
+      if (pX >= 10) pX -= 10;
+      rowY = spriteH * atlasRows.left;
+      cycle = (cycle + 1) % shots;
+    }
     ctx.clearRect(0, 0, 600, 600);
-    ctx.drawImage(img, cycle * spriteW, 0, spriteW, spriteH, 0, 0, 100, 100);
+    ctx.drawImage(img, cycle * spriteW, rowY, spriteW, spriteH, pX, pY, spriteW, spriteH);
   }, 120);
+
+  // уберём Loading... из центра страницы чтобы не маячил после загрузки
+  document.querySelector("h3[data='loading']").innerText = '';
 });
